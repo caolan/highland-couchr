dd3bd49d00208a590b86204ae1eb02f8b4153046
Caolan McMahon <caolan@caolanmcmahon.com>
2014-04-21 20:11:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+var url = require('url'),
+    follow = require('follow'),
+    couchr = require('couchr'),
+    _ = require('highland');
+
+
+var methods = [
+    'get',
+    'post',
+    'head',
+    'put',
+    'del',
+    'copy'
+];
+
+var exports = module.exports = function (base) {
+    var api = ['changes'].concat(methods).reduce(function (api, name) {
+        api[name] = function (loc /* args... */) {
+            var args = Array.prototype.slice.call(arguments);
+            args[0] = url.resolve(base, loc);
+            return exports[name].apply(this, args);
+        };
+        return api;
+    }, {});
+    api.url = base;
+    return api;
+};
+
+methods.forEach(function (name) {
+    exports[name] = _.curry(function (loc, q) {
+        return _(function (push, next) {
+            console.log(['highland-couchr', name, loc, q]);
+            couchr[name](loc, q, function (err, res, req) {
+                if (err) {
+                    console.log(['highland-couchr err', err]);
+                    push(err);
+                    push(null, _.nil);
+                }
+                else {
+                    req.body = res;
+                    push(null, req);
+                    push(null, _.nil);
+                }
+            });
+        });
+    });
+});
+
+exports.changes = function (db, q) {
+    return _(function (push, next) {
+        q = q || {};
+        if (!q.hasOwnProperty('since')) {
+            q.since = 'now';
+        }
+        q.db = db;
+        var feed = new follow.Feed(q);
+        feed.on('change', function (change) {
+            push(null, change);
+        });
+        feed.on('error', push);
+        feed.follow();
+    });
+};
